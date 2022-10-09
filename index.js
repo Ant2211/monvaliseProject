7851983bd08fb6106d0bca3147f1f2692f9b0c37
Ant2211 <turpak85@gmail.com>
2022-10-09 19:16:40 +0300

    upload

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 
 const rangeInput = document.querySelectorAll(".range-input input"),
- priceInput = document.querySelector(".price-input input"),
+ priceInput = document.querySelectorAll(".price-input input"),
  progress = document.querySelector(".slider .progress");
  let priceGap = 100;
+
+ priceInput.forEach(input =>{
+    input.addEventListener("input", e => {
+        let minVal = parseInt(priceInput[0].value);
+        let maxVal = parseInt(priceInput[1].value);
+        if((maxVal - minVal >= priceGap) &&  maxVal <= 100000){
+if(e.target.className === "input-min"){
+    rangeInput[0].value = minVal;
+    progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+}else{
+    rangeInput[1].value = maxVal;
+    progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+    }
+}
+});  
+    });
+
 rangeInput.forEach(input =>{
     input.addEventListener("input", e => {
         let minVal = parseInt(rangeInput[0].value);
-        let maxVal = parseInt (rangeInput[1].value);
+        let maxVal = parseInt(rangeInput[1].value);
         if(maxVal - minVal < priceGap){
 if(e.target.className === "range-min"){
     rangeInput[0].value = maxVal - priceGap;
@@ -16,7 +33,7 @@ if(e.target.className === "range-min"){
  }else{
     priceInput[0].value = minVal;
     priceInput[1].value = maxVal;
-progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+    progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
         progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
     }
 });  
